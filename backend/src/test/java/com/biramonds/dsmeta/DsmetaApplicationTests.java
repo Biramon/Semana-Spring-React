cbea7eb01085f30b5b195998f33acdb043027200
Ubirajara Junior <ubirajara.viegas@yahoo.com.br>
2022-09-06 22:33:50 -0300

    Project created

---
+++ backend/src/test/java/com/biramonds/dsmeta/DsmetaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.biramonds.dsmeta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsmetaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
