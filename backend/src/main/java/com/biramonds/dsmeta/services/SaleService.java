4e69204efb1ef98de8e1e42853ec625075abea33
Ubirajara Junior <ubirajara.viegas@yahoo.com.br>
2022-09-10 19:50:46 -0300

    Date select

--- backend/src/main/java/com/biramonds/dsmeta/services/SaleService.java
+++ backend/src/main/java/com/biramonds/dsmeta/services/SaleService.java
@@ -1,8 +1,12 @@
 package com.biramonds.dsmeta.services;
 
-import java.util.List;
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.ZoneId;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import com.biramonds.dsmeta.entities.Sale;
@@ -15,8 +19,16 @@ public class SaleService {
 	@Autowired
 	private SaleRepository repository;
 	
-	public List<Sale> findSales() {
-		return repository.findAll();
+	public Page<Sale> findSales(String minDate, String maxDate, Pageable pageable) {
+		
+		//Como pegar a data de hoje
+		LocalDate today = LocalDate.ofInstant(Instant.now(), ZoneId.systemDefault());
+		
+		//Utilizando expressões condicionais ternária
+		LocalDate min = minDate.equals("") ? today.minusDays(365) : LocalDate.parse(minDate);
+		LocalDate max = maxDate.equals("") ? today : LocalDate.parse(maxDate);
+		
+		return repository.findSales(min, max, pageable);
 	}
 
 }
